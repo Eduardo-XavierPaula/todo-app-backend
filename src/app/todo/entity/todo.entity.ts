32eb9f403d23f53f671518fb7ae99c3c522d3c9e
Eduardo Xavier <49372451+Eduardo-XavierPaula@users.noreply.github.com>
2024-11-03 23:00:28 -0300

    📚 Documentation: Documentação dos endpoints pelo swagger, by Eduardo Xavier

--- src/app/todo/entity/todo.entity.ts
+++ src/app/todo/entity/todo.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   CreateDateColumn,
@@ -10,15 +11,21 @@ import {
 @Entity({ name: 'todos' })
 export class TodoEntity {
   @PrimaryGeneratedColumn('uuid')
+  @ApiProperty()
   id: string;
   @Column()
+  @ApiProperty()
   task: string;
   @Column({ name: 'is_done', type: 'tinyint', width: 1 })
+  @ApiProperty()
   isDone: number;
   @CreateDateColumn({ name: 'createdAt' })
+  @ApiProperty()
   createdAt: string;
   @UpdateDateColumn({ name: 'updatedAt' })
+  @ApiProperty()
   updatedAt: string;
   @DeleteDateColumn({ name: 'deletedAt' })
+  @ApiProperty()
   deletedAt: string;
 }
